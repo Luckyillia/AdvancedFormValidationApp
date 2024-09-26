ef574813cc4393a422e0e43a5d715615424ea37f
Illia Zaichenko <10376901u@qed.local>
2024-09-26 15:28:31 +0200

    First commit

---
+++ app/src/main/java/com/example/advancedformvalidationapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.advancedformvalidationapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonDisable;
+    private TextView counter;
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
